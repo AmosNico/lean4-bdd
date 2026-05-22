058df64b6ad1563a7eab22b8f0e988cc7aecacfd
Amos Nicodemus <75745529+AmosNico@users.noreply.github.com>
2026-05-22 17:35:39 +0200

    Replace BDD.denotation by GetElem instance, automating most bounds proofs when evaluating bdds, and add BDD.DependsOn

--- Bdd/Sat.lean
+++ Bdd/Sat.lean
@@ -29,12 +29,12 @@ lemma BDD_of_CNF_nvars : (BDD_of_CNF (n := n) C).nvars ≤ n := by
   induction clauses <;> simp_all [BDD_of_CNF]
 
 lemma BDD_of_literal_correct {n} {f : Fin n → Bool} (l : Std.Sat.Literal (Fin n)) :
-    (BDD_of_literal l).denotation (by simp) (Vector.ofFn f) = (f l.1 == l.2) := by
+    (BDD_of_literal l)[Vector.ofFn f] = (f l.1 == l.2) := by
   simp [BDD_of_literal]
   split <;> simp_all
 
 lemma BDD_of_clause_correct {n} {f : Fin n → Bool} (c : Std.Sat.CNF.Clause (Fin n)) :
-    (BDD_of_clause c).denotation (by simp) (Vector.ofFn f) = Std.Sat.CNF.Clause.eval f c := by
+    (BDD_of_clause c)[Vector.ofFn f] = Std.Sat.CNF.Clause.eval f c := by
   induction c with
   | nil => simp [BDD_of_clause]
   | cons head tail ih =>
@@ -42,13 +42,12 @@ lemma BDD_of_clause_correct {n} {f : Fin n → Bool} (c : Std.Sat.CNF.Clause (Fi
     simp_rw [List.map_cons, List.foldr_cons, BDD.or_denotation, BDD_of_literal_correct]
 
 lemma BDD_of_CNF_correct {n} {f : Fin n → Bool} (C : Std.Sat.CNF (Fin n)) :
-    Std.Sat.CNF.eval f C = (BDD_of_CNF C).denotation (n := n) (by simp) (Vector.ofFn f) := by
+    Std.Sat.CNF.eval f C = (BDD_of_CNF C)[Vector.ofFn f] := by
   rcases C with ⟨⟨clauses⟩⟩
   simp only [Std.Sat.CNF.eval, List.size_toArray, List.all_toArray', BDD_of_CNF, List.map_toArray,
     List.length_map, List.foldr_toArray']
   induction clauses with
-  | nil => simp only [List.all_nil, List.map_nil, List.foldr_nil, BDD.const_denotation,
-    Function.const_apply]
+  | nil => simp only [List.all_nil, List.map_nil, List.foldr_nil, BDD.const_denotation]
   | cons head tail ih =>
     simp only [List.all_cons, List.map_cons, List.foldr_cons, BDD.and_denotation]
     rw [ih, BDD_of_clause_correct]
@@ -60,28 +59,23 @@ public instance instDecidableUnsat {n} (C : Std.Sat.CNF (Fin n)) :
     l_to_r h := by
       simp only [BDD.SemanticEquiv] at h
       contrapose h
-      rw [funext_iff]
       simp only [Std.Sat.CNF.Unsat, not_forall, Bool.not_eq_false] at h
       rcases h with ⟨f, hf⟩
       rw [BDD_of_CNF_correct] at hf
-      simp only [BDD.const_denotation, Function.const_apply, not_forall, Bool.not_eq_false]
+      simp only [BDD.const_denotation, not_forall, Bool.not_eq_false]
       use Vector.cast (by simp) (Vector.ofFn fun i : Fin (BDD_of_CNF C).nvars ↦ f ⟨i.1, lt_of_lt_of_le i.2 BDD_of_CNF_nvars⟩)
       simp only [le_refl, BDD.denotation_cast]
-      rw [BDD.denotation_take'] at hf
       rw [← hf]
-      congr 1
-      ext i hi
-      have h := Vector.getElem_extract (as := (Vector.ofFn f)) (show i < (min (BDD_of_CNF C).nvars n) - 0 by simpa)
-      simp_rw [Vector.take_eq_extract, Vector.getElem_cast, h,  Nat.zero_add, Vector.getElem_ofFn]
+      apply BDD.getElem_congr
+      grind only [= Fin.getElem_fin, = Vector.getElem_ofFn]
     r_to_l h1 := by
       simp only [Std.Sat.CNF.Unsat] at h1
-      simp only [BDD.SemanticEquiv]
-      ext I
-      simp only [BDD.const_denotation, Function.const_apply]
+      simp only [BDD.SemanticEquiv, BDD.const_denotation]
+      intro I
       have h2:= h1 (fun i ↦ if hi : i < (max (BDD_of_CNF C).nvars (BDD.const false).nvars) then I[i] else false)
       simp only [BDD.const_nvars, Nat.zero_le, sup_of_le_left, BDD_of_CNF_correct] at h2
       refine Eq.trans ?_ h2
-      apply BDD.denotation_congr
+      apply BDD.getElem_congr
       simp
 
 -- #eval Std.Sat.CNF.eval (fun _ : Nat ↦ true) ⟨#[]⟩
